cbd4dc2294e740005fc1ea7d79418d315c1868ca
Pamela <pamelafumagalli85@gmail.com>
2023-01-16 21:32:55 -0300

    formulario veterinaria

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,42 @@
+import Paciente from "./Paciente";
+import "../css/ListadoPacientes.css";
+
+const ListadoPacientes = ({ pacientes, setPaciente }) => {
+  return (
+    <div
+      className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll"
+      id="listado-scroll"
+    >
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold ">
+              Pacientes y Citas
+            </span>
+          </p>
+          {pacientes.map((paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold ">
+              Y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
